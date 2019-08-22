12f4604bd5d79301560a7048450afe647cfc2b5a
FRex <rex2k8@gmail.com>
2019-08-23 01:13:41 +0200

    added wmain

--- binarypic.c
+++ binarypic.c
@@ -4,7 +4,7 @@
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_STATIC
-#define STBIW_WINDOWS_UTF8
+#define STBI_WINDOWS_UTF8
 #include "stb_image_write.h"
 
 #include <stdio.h>
@@ -52,7 +52,30 @@ static int calculate_image_sizes(size_t s, int * x, int * y)
     return 0;
 }
 
-int main(int argc, char ** argv)
+#ifdef _MSC_VER
+/*for MultiByteToWideChar */
+#include <Windows.h>
+#endif
+
+static FILE * my_utf8_fopen_rb(const char * fname)
+{
+#ifndef _MSC_VER
+    return fopen(fname, "rb");
+#else
+    FILE * ret;
+    const int utf16chars = strlen(fname) + 5;
+    wchar_t * fname16 = (wchar_t*)calloc(utf16chars, sizeof(wchar_t));
+    if(!fname16)
+        return NULL;
+
+    MultiByteToWideChar(CP_UTF8, 0, fname, -1, fname16, utf16chars);
+    ret = _wfopen(fname16, L"rb");
+    free(fname16);
+    return ret;
+#endif
+}
+
+static int my_utf8_main(int argc, char ** argv)
 {
     void * buff;
     size_t buffsize = 4096 * 4096;
@@ -70,7 +93,7 @@ int main(int argc, char ** argv)
         return 1;
     }
 
-    binary = fopen(argv[1], "rb");
+    binary = my_utf8_fopen_rb(argv[1]);
     if(!binary)
     {
         free(buff);
@@ -78,7 +101,7 @@ int main(int argc, char ** argv)
         return 1;
     }
 
-    read = fread(buff, 1, buffsize + 1, binary);
+    read = fread(buff, 1, buffsize + 1, binary); /* TODO: retry until eof? */
     fclose(binary);
     if(read > buffsize)
     {
@@ -89,7 +112,7 @@ int main(int argc, char ** argv)
 
     if(calculate_image_sizes(read, &x, &y))
     {
-        stbi_write_png(argv[2], x, y, 1, buff, 0); /*TODO: check this*/
+        stbi_write_png(argv[2], x, y, 1, buff, 0); /* TODO: check this*/
     }
     else
     {
@@ -98,3 +121,51 @@ int main(int argc, char ** argv)
     free(buff);
     return 0;
 }
+
+#ifndef _MSC_VER
+
+int main(int argc, char ** argv)
+{
+    return my_utf8_main(argc, argv);
+}
+
+#else
+
+/* for wcslen */
+#include <wchar.h>
+
+int wmain(int argc, wchar_t ** argv)
+{
+    int i, retcode;
+    char ** utf8argv = (char **)calloc(argc + 1, sizeof(char*));
+    if(!utf8argv)
+    {
+        fputs("calloc error in wmain\n", stderr);
+        return 1;
+    }
+
+    retcode = 0;
+    for(i = 0; i < argc; ++i)
+    {
+        const size_t utf8len = wcslen(argv[i]) * 3 + 10;
+        utf8argv[i] = (char*)calloc(utf8len, 1);
+        if(!utf8argv[i])
+        {
+            retcode = 1;
+            fputs("calloc error in wmain\n", stderr);
+            break;
+        }
+        stbiw_convert_wchar_to_utf8(utf8argv[i], utf8len, argv[i]);
+    }
+
+    if(retcode == 0)
+        retcode = my_utf8_main(argc, utf8argv);
+
+    for(i = 0; i < argc; ++i)
+        free(utf8argv[i]);
+
+    free(utf8argv);
+    return retcode;
+}
+
+#endif /* _MSC_VER */
